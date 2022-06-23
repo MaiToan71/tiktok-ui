b4f112048df82287faed3c2e4d3017cae1926de4
iammaitoan <maitoan97hd@gmail.com>
2022-06-23 15:03:45 +0700

    config route

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+// Layouts
+
+// Pages
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+
+// Public routes
+const publicRoutes = [
+    { path: "/", component: Home },
+    { path: "/following", component: Following },
+    { path: "/profile", component: Profile },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
